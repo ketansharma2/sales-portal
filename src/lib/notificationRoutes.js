647be93ed00299b64d7508dce0320475ec9c74b4
developer-maven-02 <jrdeveloper.mavenjobs@gmail.com>
2026-06-30 10:46:07 +0530

    change rc to recruiter and remove console logs

--- src/lib/notificationRoutes.js
+++ src/lib/notificationRoutes.js
@@ -1,7 +1,7 @@
 // src/lib/notificationRoutes.js
 
 export const getNotificationRoute = (userData) => {
-  console.log("userData:",userData);
+
   if (!userData) return '/notifications';
 
   let role = userData.current_role?.toLowerCase();
@@ -31,7 +31,7 @@ export const getNotificationRoute = (userData) => {
     'crm',
     'revenue',
   ];
-  console.log("sector,role", sector,role);
+ 
   if (sector && sectorRoles.includes(role)) {
     return `/${sector}/${role}/notifications`;
   }
